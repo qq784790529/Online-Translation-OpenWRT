1740d53a993eb74f40f26718ee790df131293a16
My surname is Ou <784790529@qq.com>
2026-08-20 21:14:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -38,3 +38,6 @@ echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 
 # 3ginfo-lite 专注于显示信号强度、网络类型等关键信息
 # echo 'src-git 3ginfo https://github.com/4IceG/luci-app-3ginfo-lite.git' >>feeds.conf.default
+
+# RTL驱动
+echo 'src-git rtw88usb https://github.com/henkv1/rtw88-usb-openwrt.git;23.05' >>feeds.conf.default
